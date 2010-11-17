338bcce9fdd69b23a86b6fb61e377daf352dea65
Oliver Charles <oliver.g.charles@googlemail.com>
2010-11-17 16:19:31 +0000

    Fail verification through Test::Builder

--- lib/Test/Mockito/Mock.pm
+++ lib/Test/Mockito/Mock.pm
@@ -4,6 +4,7 @@ use namespace::autoclean;
 
 use MooseX::Types::Moose qw( ArrayRef Int );
 use Moose::Util qw( find_meta );
+use Test::Builder;
 
 our $STATE_RECORD = 1;
 our $STATE_VERIFY = 2;
@@ -23,20 +24,31 @@ has 'state' => (
 
 our $AUTOLOAD;
 
+my $Test = Test::Builder->new();
+
 sub AUTOLOAD {
     my $method = $AUTOLOAD;
     my $self = shift;
     my $meta = find_meta($self);
     my $state = $meta->get_attribute('state')->get_value($self);
     my $invocations = $meta->get_attribute('invocations')->get_value($self);
+    my $invocation = [ $method, \@_ ];
 
     if ($state == $STATE_RECORD) {
-        push @$invocations, [ $method, \@_ ];
+        push @$invocations, $invocation;
     }
     elsif ($state == $STATE_VERIFY) {
         my $top = shift(@$invocations);
-        die "Verification error"
-            unless $top && $top->[0] eq $method;
+        unless ($top && $top->[0] eq $method) {
+            $Test->ok(0, 'Verification failed');
+            $Test->diag(
+                sprintf "Expected:\t%s\nGot:\t\t%s",
+                    (defined $top
+                        ? $top->[0]
+                        : '(no more calls)'),
+                    $invocation->[0]
+            );
+        }
     }
 }
 
