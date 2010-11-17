67e48e7bef7f909559674f598cb1103bd48e06bc
Oliver Charles <oliver.g.charles@googlemail.com>
2010-11-17 17:14:54 +0000

    Stepping back to match Mockito's methodology

--- lib/Test/Mockito/Mock.pm
+++ lib/Test/Mockito/Mock.pm
@@ -9,55 +9,27 @@ use MooseX::Types::Moose qw( ArrayRef Int );
 use Moose::Util qw( find_meta );
 use Test::Builder;
 
-our $STATE_RECORD = 1;
-our $STATE_VERIFY = 2;
-
-has 'expectations' => (
+has 'invocations' => (
     isa => ArrayRef,
     is => 'bare',
     traits => [ 'Array' ],
     default => sub { [] }
 );
 
-has 'state' => (
-    isa => Int,
-    is => 'bare',
-    default => $STATE_RECORD
-);
-
 our $AUTOLOAD;
-
-my $Test = Test::Builder->new();
 
 sub AUTOLOAD {
     my $method = $AUTOLOAD;
     my $self = shift;
     my $meta = find_meta($self);
-    my $state = $meta->get_attribute('state')->get_value($self);
-    my $expectations = $meta->get_attribute('expectations')->get_value($self);
-    my %args = (
+    my $invocations = $meta->get_attribute('invocations')->get_value($self);
+    my $invocation = Invocation->new(
         method_name => $method,
         arguments => \@_
     );
 
-    if ($state == $STATE_RECORD) {
-        my $expectation = Expectation->new(%args);
-        push @$expectations, $expectation;
-    }
-    elsif ($state == $STATE_VERIFY) {
-        my $expectation = shift(@$expectations);
-        my $invocation = Invocation->new(%args);
-
-        my $satisfied = $expectation && $expectation->satisfied_by($invocation);
-        $Test->ok($satisfied, 'Expecting ' . $expectation->as_string);
-        $Test->diag(
-            sprintf "Expected:\t%s\nGot:\t\t%s",
-                (defined $expectation
-                    ? $expectation->as_string
-                    : '(no more calls)'),
-                $invocation->as_string
-        ) unless $satisfied;
-    }
+    push @$invocations, $invocation;
+    return;
 }
 
 1;
--- t/basic.t
+++ t/basic.t
@@ -1,17 +1,11 @@
 use strict;
 use warnings;
-use Test::Mockito qw( history mock verify );
+use Test::Mockito qw( when mock verify );
 use Test::More;
 
-my $mock = mock;
-
-$mock->do_something;
-$mock->explode('rapidly');
-
-verify($mock);
-$mock->do_something;
-$mock->explode;
-$mock->boom;
+my $mock = mock();
+print $mock->foo;
 
 pass;
 done_testing;
+
